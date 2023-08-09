9b767b5ebef85b1fc148b5a8d5274e1dfeb05d48
itsnichi4 <120479794+itsnichi4@users.noreply.github.com>
2023-08-09 22:04:43 +0300

    add svelte template, create components

---
+++ CVApp/src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import AddPersonalDetails from './components/addPersonalDetails.svelte';
+</script>
+
+<main>
+  <AddPersonalDetails />
+</main>
+
+<style>
+
+</style>
---
+++ CVApp/src/components/addPersonalDetails.svelte
@@ -0,0 +1,49 @@
+<script>
+    let details = {
+        Name: "",
+        Email: "",
+        Phone: "",
+        Address: "",
+    };
+
+    let save = false;
+    let show = false;
+    let edit = false;
+
+    function changeSave() {
+        save = !save;
+        changeEdit();
+        show = true;
+    }
+
+    function changeEdit() {
+        edit = !edit;
+        show = false;
+    }
+</script>
+
+{#if show && details.Name && details.Email && details.Phone && details.Address}
+    <div>
+        <p>Full Name: {details.Name}</p>
+        <p>Email: {details.Email}</p>
+        <p>Phone Number: {details.Phone}</p>
+        <p>Address: {details.Address}</p>
+    </div>
+{:else}
+    <div>
+        <p>Personal Details:</p>
+    </div>
+{/if}
+
+{#if !edit}
+    <button on:click={changeEdit}>Edit</button>
+{/if}
+{#if edit}
+    {#each Object.entries(details) as [field] (field)}
+        <div>
+            <label for={field}>{field}:</label>
+            <input type="text" id={field} bind:value={details[field]} />
+        </div>
+    {/each}
+    <button on:click={changeSave}>Save</button>
+{/if}
